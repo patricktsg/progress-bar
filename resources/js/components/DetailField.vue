37223bc4548f0dd20021d8a9f24f8def376b0851
Patrick Corkery <patrick@thesmartgroup.ie>
2019-04-02 18:59:53 +0100

    Added in more breakpoints in 10 increments and more shades for the steps

--- resources/js/components/DetailField.vue
+++ resources/js/components/DetailField.vue
@@ -14,18 +14,35 @@ export default {
     props: ['resource', 'resourceName', 'resourceId', 'field'], 
     methods: {
         setBarColor: function (value) {
-            if (value < 50 ) {
+            if (value < 10) {
+                return '#cc1f1a';
+            } 
+            else if (value < 20 ) {
+                return '#e3342f';
+            }
+            else if (value < 30 ) {
                 return '#ef5753';
-            } else if (value <= 60) {
+            } 
+            else if (value < 40 ) {
                 return '#de751f';
-            } else if (value <= 70) {
+            }
+            else if (value < 50 ) {
                 return '#f6993f';
-            } else if (value <= 80) {
+            } 
+            else if (value <= 60) {
                 return '#faad63';
-            } else if (value <= 90) {
+            } 
+            else if (value <= 70) {
+                return '#a2f5bf';
+            } 
+            else if (value <= 80) {
                 return '#51d88a';
-            } else if (value = 100) {
+            } 
+            else if (value <= 90) {
                 return '#38c172';
+            } 
+            else if (value = 100) {
+                return '#1f9d55';
             }
         }
     }
